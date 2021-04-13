4f835055896b5e83b3e6019ae795aadcc9e61bf0
Ribodou <luc.robidou@gmail.com>
2021-04-13 14:57:44 +0200

    cleaner functions

--- analyseResponse.py
+++ analyseResponse.py
@@ -1,61 +1,55 @@
 import matplotlib.pyplot as plt
 
+START = 1000
+N = 1000000
 
-def read_file0n(filename, n, xlimit):
+
+def read_file0n(filename, n):
     les_x = []
     les_y = []
     with open(filename, "r") as fichier:
         for ligne in fichier:
             for i, x in enumerate(ligne):
-                if x == "0":
-                    # contenu.append(0)
-                    pass
-                elif x == "1":
+                if i < START:
+                    continue
+                if x == "1":
                     les_x.append(i)
                     les_y.append(n)
-                else:
-                    print("erreur, x=", x)
-                if i >= xlimit:
+                if i >= START + N:
                     return les_x, les_y
     return les_x, les_y
 
 
-def diff(filename1, filename2, n, xlimit):
+def diff(filename1, filename2, n):
     les_x = []
     les_y = []
     with open(filename1, "r") as fichier1, open(filename2, "r") as fichier2:
         for ligne1, ligne2 in zip(fichier1, fichier2):
             for i, (x1, x2) in enumerate(zip(ligne1, ligne2)):
-                if x1 == x2:
-                    # contenu.append(0)
-                    pass
-                else:
+                if i < START:
+                    continue
+                if x1 != x2:
                     les_x.append(i)
                     les_y.append(n)
-                # else:
-                #     print("erreur, x=", x)
-                if i >= xlimit:
+                if i >= START + N:
                     return les_x, les_y
     return les_x, les_y
 
 
-if __name__ == "__main__":
-    nmax = 1000000
-    les_x_truth, les_y_truth = read_file0n("truth.txt", 1, nmax)
-    les_x_responseQuery, les_y_responseQuery = read_file0n("responseQuery.txt", 2, nmax)
-    les_x_responseQTF, les_y_responseQTF = read_file0n("responseQTF.txt", 3, nmax)
-    les_x_responseQTFPlus, les_y_responseQTFPlus = read_file0n(
-        "responseQTFPlus.txt", 4, nmax
-    )
-    les_x_truthPlusZ, les_y_truthPlusZ = read_file0n("truthPlusK.txt", 5, nmax)
+def myplot(filename, n):
+    plt.plot(*read_file0n(filename, n), "o", markersize=2, label=filename)
 
-    plt.plot(les_x_truth, les_y_truth, "o", markersize=2)
-    plt.plot(*diff("truth.txt", "responseQuery.txt", 1.5, nmax), "o", markersize=2)
-    plt.plot(les_x_responseQuery, les_y_responseQuery, "o", markersize=2)
-    plt.plot(les_x_responseQTF, les_y_responseQTF, "o", markersize=2)
-    plt.plot(les_x_responseQTFPlus, les_y_responseQTFPlus, "o", markersize=2)
+
+if __name__ == "__main__":
+    myplot("truthQuery.txt", 1)
+    # plt.plot(*diff("truth.txt", "responseQuery.txt", 1.5, nmax), "o", markersize=2)
+    # plt.plot(*read_file0n("responseQuery.txt", 2, nmax), "o", markersize=2)
+
+    myplot("responseQTF.txt", 3)
+    myplot("responseQTFKPlusZ.txt", 4)
     plt.plot(
-        *diff("truthPlusK.txt", "responseQTFPlus.txt", 4.5, nmax), "o", markersize=2
+        *diff("truthKPlusZQuery.txt", "responseQTFKPlusZ.txt", 4.5), "o", markersize=2
     )
-    plt.plot(les_x_truthPlusZ, les_y_truthPlusZ, "o", markersize=2)
+    myplot("truthKPlusZQuery.txt", 5)
+    plt.legend(loc="best")
     plt.show()
