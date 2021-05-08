5e5c527d33e4f4b2dcca838322e305b2e4a2ba0e
Lucas Robidou <lucas.robidou@inria.fr>
2021-05-08 17:36:36 +0200

    WIP: clean+force value parameters

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,12 @@
 #include "libraries/querier/querier.hpp"
 #include "libraries/utils/argsUtils.hpp"
 #include "libraries/utils/utils.hpp"
-void read_fastq(std::string filename) {
-    std::ifstream myfile(filename);
-    zstr::istream is(myfile);
-    std::string s;
-    unsigned long long i = 0;
-    while (getline(is, s)) {
-        std::cout << i << std::endl;
-        i++;
-    }
-}
 
 int main(int argc, char* argv[]) {
     const unsigned numHashes = 1;  // number of hash functions
 
     cxxopts::ParseResult arguments = parseArgv(argc, argv);
     const auto& [input_filenames, queryFile, k, z, epsilonPercent] = getArgs(arguments);
-    //read_fastq(input_filenames[0]);
 
     std::string querySeq = extractContentFromFastqGz(queryFile);
 
@@ -39,8 +28,10 @@ int main(int argc, char* argv[]) {
         auto t2 = std::chrono::high_resolution_clock::now();
 
         for (unsigned long long z_iter = 0; z_iter < z; z_iter++) {
-            // robin_hood::unordered_set<std::string> truthSmallK = truth::indexFastas(input_filenames, k_iter - z);
             for (double epsilonPercent_iter = 0.5; epsilonPercent_iter <= epsilonPercent; epsilonPercent_iter += 0.5) {
+                epsilonPercent_iter = 5;
+                z_iter = 5;
+
                 QTF_internal::printContext(k_iter, z_iter, epsilonPercent_iter);
                 const auto& [truthSmallK, smallFilter, timeTakenMs, sizeOfBloomFilter] = QTF::indexFastqGz(input_filenames, numHashes, k_iter, epsilonPercent_iter, z_iter);
                 auto t3 = std::chrono::high_resolution_clock::now();
@@ -57,9 +48,9 @@ int main(int argc, char* argv[]) {
                 delete smallFilter;
 
                 std::cout << "]" << std::endl;
-//toFileTXT("qtfonbf.txt", QTFOnBloomFilter);
-//toFileTXT("truth.txt", bigTruth);
-//toFileTXT("qtfOnTruth.txt", QTFOnSmallTruth);
+                //toFileTXT("qtfonbf.txt", QTFOnBloomFilter);
+                //toFileTXT("truth.txt", bigTruth);
+                //toFileTXT("qtfOnTruth.txt", QTFOnSmallTruth);
                 return 0;
             }
         }
