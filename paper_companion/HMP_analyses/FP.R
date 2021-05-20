ac9d16060a90ed949caee6d347d48a02af21cbc1
Pierre Peterlongo <pierre.peterlongo@inria.fr>
2021-05-20 17:53:27 +0200

    version fpr only

--- paper_companion/HMP_analyses/FP.R
+++ paper_companion/HMP_analyses/FP.R
@@ -1,5 +1,5 @@
 
-pdf(file="fpr_time.pdf",width=11,height=8)
+pdf(file="fpr.pdf",width=11,height=8)
 resetPar <- function() {
   dev.new()
   op <- par(no.readonly = TRUE)
@@ -17,7 +17,7 @@ data = read.table("data_FP_K31_hmp.txt", head=T)
 datime = read.table("data_time_K31_hmp.txt", head=T)
 data$FP_BFk = data$findere_FPR-data$construction_FPR
 plot(data$AMQ_FPR*100~data$z,xlab="z value",ylab="FP rate (%)", lwd=2, pch=15, col="blue", 
-     bg = "blue", type="o",  mgp=c(3,1,0), cex=2, ylim=c(0,7), xlim=c(0,10), main="False positive rate, depending on z, K=31", cex.main = 2, cex.lab = 2, cex.axis = 2)
+     bg = "blue", lty=2, type="o",  mgp=c(3,1,0), cex=2, ylim=c(0,7), xlim=c(0,10), main="False positive rate, depending on z, K=31", cex.main = 2, cex.lab = 2, cex.axis = 2)
 
 
 par(new = TRUE, mgp=c(3,1,0))
@@ -30,28 +30,18 @@ plot(data$construction_FPR*100~data$z,type="o",axes=FALSE,xlab="",ylab="",pch=21
 
 
 legend( x="topleft", 
-        legend=c("Bloom Filter (5%)","findere", "findere construction FP", "findere query time (s)"), 
-        col=c("blue","red", "orange", "black"), lty=c(1,1,1,2),
-        pch=c(15,19, 21, 1), lwd=2,merge=TRUE, bty = "n", cex=c(2,2,2) )
-
-# TIME
-par(new = TRUE, mgp=c(3,1,0))
-plot(datime$findere/1000~datime$z,type="b",axes=FALSE,xlab="",ylab="",pch=1, cex=2, col="black", xlim=c(0,5), ylim=c(0,60), lty=2)
-mtext("findere query time (s)",side=4,line=4, cex=2.5)
-axis(4, ylim=c(0,60), las=1, cex.lab=2, mgp=c(3,1,0))
+        legend=c("Bloom Filter (5%)","findere", "findere construction FP"), 
+        col=c("blue","red", "orange"), lty=c(2,1,1),
+        pch=c(15,19, 21), lwd=2,merge=TRUE, bty = "n", cex=c(2,2,2) )
 
 
 
 
 
 # INCRUSTATION
-par(new=TRUE, oma =c(14.6,23.6,7.2,3), cex.axis=1, cex.lab=1)
-mywhite <- rgb(255, 255, 255, max = 255, alpha = 225, names = "white")
-rect(par("usr")[1], par("usr")[3],
-     par("usr")[2], par("usr")[4],
-     col = mywhite) # Color
+par(new=TRUE, oma =c(5.6,24.6,12.2,3), cex.axis=1, cex.lab=1)
 layout(matrix(1:1,1))
-par(new = TRUE)
+
 
 
 #plot(rnorm(100), runif(100), col="blue", xlab="", ylab="")
