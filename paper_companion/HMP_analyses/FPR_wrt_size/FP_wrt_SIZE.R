1a66eb49c6ad57aa7b6f67a14c715c442158ae99
Pierre Peterlongo <pierre.peterlongo@inria.fr>
2021-05-21 16:29:55 +0200

    results with respect to size

---
+++ paper_companion/HMP_analyses/FPR_wrt_size/FP_wrt_SIZE.R
@@ -0,0 +1,48 @@
+
+pdf(file="fpr_size.pdf",width=11,height=8)
+resetPar <- function() {
+  dev.new()
+  op <- par(no.readonly = TRUE)
+  dev.off()
+  op
+}
+resetPar()
+
+
+dev.new()
+
+
+
+dev.off()
+#par(mar=c(5,5,3,6), cex.axis=2, cex.lab=2, cex.main = 2)
+data = read.table("data_FP_wrt_size.txt", head=T)
+data$sizeGiga = data$size/8589934592
+plot(data$FPR_BF~data$sizeGiga,xlab="Bloom Filter size (MB)",ylab="FP rate (%) - log scale", pch=15, col="blue", log="y",
+     bg = "blue", type="l",  lty=2, mgp=c(3,1,0), cex=2, main="False positive rate, depending on the Bloom filter size, K=31", cex.main = 1, cex.lab = 1, cex.axis = 1)
+
+
+
+par(new = TRUE)
+segments(-1000, 0.1, 20, 0.1,
+         col = par("fg"), lty = 3, lwd = par("lwd"), cex=2)
+
+segments(0.4, 0.000001,  0.4, 0.2,
+         col = par("fg"), lty = 3, lwd = par("lwd"), cex=2)
+segments(16, 0.000001, 16, 0.2,
+         col = par("fg"), lty = 3, lwd = par("lwd"), cex=2)
+
+par(new = TRUE, mgp=c(3,1,0))
+plot(data$FPR_findere~data$sizeGiga,type="l",axes=FALSE,xlab="",ylab="",pch=19, col="red",  cex=2, log="y")
+
+
+
+legend( x="top", 
+        legend=c("Bloom Filter","findere"), 
+        col=c("blue","red"), lty=c(2,1),
+        lwd=2, merge=TRUE, bty = "n", cex=c(1,1) )
+
+
+dev.off()
+
+
+
