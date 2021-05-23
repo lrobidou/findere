da521014ff1ee19332da128a406a84f30260a1da
Pierre Peterlongo <pierre.peterlongo@inria.fr>
2021-05-23 09:40:33 +0200

    correct fpr size curve

--- paper_companion/HMP_analyses/FPR_wrt_size/FP_wrt_SIZE.R
+++ paper_companion/HMP_analyses/FPR_wrt_size/FP_wrt_SIZE.R
@@ -20,9 +20,9 @@ data = read.table("data_FP_wrt_size.txt", head=T)
 data$sizeGiga = data$size/8000000000
 
 
-plot(data$FPR_findere~data$sizeGiga,xlab="Bloom Filter size (GB)",ylab="FP rate (%) - log scale", pch=15, col="blue",log="y",yaxt="n",xaxt="n",
-     bg = "blue", type="l",  lty=2,  cex=2, main="False positive rate, depending on the Bloom filter size, K=31", cex.main = 1, cex.lab = 1, cex.axis = 1)
-points(data$FPR_BF~data$sizeGiga,type="l",axes=FALSE,xlab="",ylab="",pch=19, col="red",  cex=1, log="y")
+plot(data$FPR_findere~data$sizeGiga,xlab="Bloom Filter size (GB)",ylab="FP rate (%) - log scale", pch=15, col="red",log="y",yaxt="n",xaxt="n",
+     bg = "red", type="l",  cex=2, main="False positive rate, depending on the Bloom filter size, K=31", cex.main = 1, cex.lab = 1, cex.axis = 1)
+points(data$FPR_BF~data$sizeGiga,type="l",axes=FALSE,xlab="",ylab="",pch=19, lty=2, col="blue",  cex=1, log="y")
 
 yticks<-c(0.1,0.5,1,5,10, 50)
 axis(2,at=yticks,labels=yticks)
