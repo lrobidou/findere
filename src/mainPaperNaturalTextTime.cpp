b276d0101434b531196b51224925dd4dd2561f3e
Lucas Robidou <lucas.robidou@inria.fr>
2021-05-26 10:39:28 +0200

    copy main Paper

--- src/mainPaperNaturalTextTime.cpp
+++ src/mainPaperNaturalTextTime.cpp
@@ -14,6 +14,7 @@
 int main(int argc, char* argv[]) {
     // in Python,after import numpy as np: [int(x) for x in np.linspace(100, 2674199464*3, 100)] gives you this:
     std::vector<unsigned long long> zs = {0, 1, 2, 3, 4, 5, 10};
+    bool canonical = false;
 
     unsigned long long K = 31;
     // unsigned long long z = 3;
@@ -29,42 +30,56 @@ int main(int argc, char* argv[]) {
 
     std::string querySeq = extractContentFromText(queryFile);
 
-    robin_hood::unordered_set<std::string> truthBigK = truth::indexText(input_filenames, K, false);
-    std::vector<bool> bigTruth = truth::queryTruth(truthBigK, querySeq, K);
-    double epsilonPercent = 5.0;
-
-    const auto& [normalfilter, sizeSimpleFilter] = QTF_internal::indexTextGivenTruth(input_filenames, truthBigK, numHashes, K, epsilonPercent, false);
+    std::cout << "[" << std::endl;
+    unsigned long long k_iter = K;
+    robin_hood::unordered_set<std::string> truthBigK;
+    auto t0 = std::chrono::high_resolution_clock::now();
+    truthBigK = truth::indexText(input_filenames, k_iter, canonical);
 
     auto t1 = std::chrono::high_resolution_clock::now();
-    std::vector<bool> noFindereimpleQuery = noQTF::query(normalfilter, querySeq, K);
+    std::vector<bool> bigTruth = truth::queryTruth(truthBigK, querySeq, k_iter);
     auto t2 = std::chrono::high_resolution_clock::now();
+    double epsilonPercent_iter = 5;
+    bf::basic_bloom_filter* normalfilter = nullptr;
+    unsigned long long numberOfIndexedElements = 0;
+    std::tie(normalfilter, numberOfIndexedElements) = QTF_internal::indexTextGivenTruth(input_filenames, truthBigK, numHashes, k_iter, epsilonPercent_iter, canonical);
 
-    for (const auto& z : zs) {
-        const auto& [truthSmallK, smallFilter, timeTakenMs, sizeOfBloomFilterFindere] = QTF::indexText(input_filenames, numHashes, K, epsilonPercent, z, false);
+    auto t6 = std::chrono::high_resolution_clock::now();
+    std::vector<bool> noQTFSimpleQuery = noQTF::query(normalfilter, querySeq, k_iter);
+    auto t7 = std::chrono::high_resolution_clock::now();
+    for (auto z_iter : zs) {
+        // z_iter = 5;
+        robin_hood::unordered_set<std::string> truthSmallK;
+        bf::basic_bloom_filter* smallFilter = nullptr;
+        int timeTakenMs;
+        unsigned long long sizeOfBloomFilter;
+
+        QTF_internal::printContext(k_iter, z_iter, epsilonPercent_iter);
+
+        std::tie(truthSmallK, smallFilter, timeTakenMs, sizeOfBloomFilter) = QTF::indexText(input_filenames, numHashes, k_iter, epsilonPercent_iter, z_iter, canonical);
 
         auto t3 = std::chrono::high_resolution_clock::now();
-        std::vector<bool> findereOnBloomFilter = QTF::query(smallFilter, querySeq, K, z, true);
+        std::vector<bool> QTFOnBloomFilter = QTF::query(smallFilter, querySeq, k_iter, z_iter);
         auto t4 = std::chrono::high_resolution_clock::now();
+        std::vector<bool> QTFOnBloomFilterSkip = QTF::query(smallFilter, querySeq, k_iter, z_iter, true);
+        auto t5 = std::chrono::high_resolution_clock::now();
 
-        QTF_internal::printContext(K, z, epsilonPercent);
-        std::cout << "        \"time\": {" << std::endl;
-        std::cout << "            \"computeBf\": " << timeTakenMs << "," << std::endl;
-        std::cout << "            \"queryBfSkip\": " << std::chrono::duration_cast<std::chrono::milliseconds>(t4 - t3).count() << "," << std::endl;
-        std::cout << "            \"queryNormalFilter\": " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << std::endl;
-        std::cout << "        }," << std::endl;
-        QTF_internal::printScore(QTF_internal::getScore(bigTruth, findereOnBloomFilter), "findere", false, sizeOfBloomFilterFindere);
-        QTF_internal::printScore(QTF_internal::getScore(bigTruth, noFindereimpleQuery), "normalfilter", true, sizeSimpleFilter);
-        std::cout << "}";
-        if (z != 10) {
+        std::vector<bool> QTFOnSmallTruth = QTF::query(truthSmallK, querySeq, k_iter, z_iter);
+
+        QTF_internal::printTime(t0, t1, t2, t3, t4, t5, t6, t7, timeTakenMs);
+        QTF_internal::printScore(QTF_internal::getScore(bigTruth, QTFOnBloomFilter), "queryBF", false, sizeOfBloomFilter);
+        QTF_internal::printScore(QTF_internal::getScore(bigTruth, QTFOnBloomFilterSkip), "queryBFSkip", false, sizeOfBloomFilter);
+        QTF_internal::printScore(QTF_internal::getScore(bigTruth, QTFOnSmallTruth), "resultsOnSmallTruth", false);
+        QTF_internal::printScore(QTF_internal::getScore(bigTruth, noQTFSimpleQuery), "normalfilter", true, numberOfIndexedElements);
+
+        std::cout << "    }";
+        if (z_iter < 10) {
             std::cout << ",";
         }
         std::cout << std::endl;
 
         delete smallFilter;
-        delete normalfilter;
     }
-
     std::cout << "]" << std::endl;
-
     return 0;
 }
