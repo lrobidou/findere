2a60ad4449579348ebdc58904baa25e5b0172103
Lucas Robidou <lucas.robidou@inria.fr>
2021-05-27 09:47:59 +0200

    index gz too

--- src/libraries/utils/utils.cpp
+++ src/libraries/utils/utils.cpp
@@ -12,19 +12,16 @@ std::string changeFilenameExtensionIfAnyOrAddOne(std::string filename, std::stri
 }
 
 std::string extractContentFromFasta(std::string filename) {
-    std::ifstream myfile(filename);
+    std::ifstream myfilegz(filename);
+    zstr::istream myfile(myfilegz);
+    // std::ifstream myfile(filename);
     std::string line;
     std::string content;
-    if (myfile.is_open()) {
-        while (std::getline(myfile, line)) {
-            if ((line[0] != '>') && (line[0] != '#')) {
-                content += line;
-            }
+
+    while (std::getline(myfile, line)) {
+        if ((line[0] != '>') && (line[0] != '#')) {
+            content += line;
         }
-        myfile.close();
-    } else {
-        std::cerr << "The file " << filename << " does not exist." << std::endl;
-        exit(1);
     }
     return content;
 }
