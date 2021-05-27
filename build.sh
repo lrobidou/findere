60c5bd22863a69af7c6cc8e8ae2beb482abd169f
Lucas Robidou <lucas.robidou@inria.fr>
2021-05-27 17:17:00 +0200

    turn on compilation of everything only on demand

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ cmake ..
 make -j4
 cd ..
 mkdir -p bin
-cp build/src/paper bin/paper
-cp build/src/paperFixedSize bin/paperFixedSize
-cp build/src/paperKVariation bin/paperKVariation
-cp build/src/paperFixedSize bin/paperFixedSize
-cp build/src/paperNaturalText bin/paperNaturalText
-cp build/src/paperNaturalTextTime bin/paperNaturalTextTime
+cp build/src/paper bin/paper 2>/dev/null || :
+cp build/src/paperFixedSize bin/paperFixedSize 2>/dev/null || :
+cp build/src/paperKVariation bin/paperKVariation 2>/dev/null || :
+cp build/src/paperFixedSize bin/paperFixedSize 2>/dev/null || :
+cp build/src/paperNaturalText bin/paperNaturalText 2>/dev/null || :
+cp build/src/paperNaturalTextTime bin/paperNaturalTextTime 2>/dev/null || :
 cp build/src/query bin/findere_query
 cp build/src/index bin/findere_index
