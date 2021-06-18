8813bcce64c8e0d33266e068dd7fef1009315768
Lucas Robidou <lucas.robidou@inria.fr>
2021-06-18 16:08:34 +0200

    une fonction propore pour les FPR theorique

--- loop.py
+++ loop.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 
 def read_file(filename):
@@ -41,18 +42,28 @@ def get_m_and_n(filename):
     return bit_per_elem, fpr_mini
 
 
+def plot_theoretical_BF_optimal(max_bpe: int, label, options):
+    bit_per_elem = []
+    theoretical_fpr = []
+    for bpe in range(1, max_bpe + 1):
+        bit_per_elem.append(bpe)
+        nb_hashes = bpe * math.log(2)
+        th_fpr = 100 * math.pow(1 - math.exp(-nb_hashes / bpe), nb_hashes)
+        theoretical_fpr.append(th_fpr)
+    plt.plot(bit_per_elem, theoretical_fpr, options, label=label)
+
+
 def main():
     plot_file("bf1.txt", "BF (one hash function)", "x-")
     plot_file("bfopti.txt", "BF (optimal hash function)", "x--")
     plot_file("findere1.txt", "findere (one hash function)", "o-")
     plot_file("findereopti.txt", "findere (otpimal hash function)", "o--")
+    plot_theoretical_BF_optimal(10, "Theoretical optimal BF", "+-")
     plt.title("FPR wrt number of bits per element")
-    les_x, les_y = get_m_and_n("bf1.txt")
-    print(les_x, les_y)
-    plt.plot(les_x, les_y)
     plt.xlabel("bits / element")
     plt.ylabel("FPR (%)")
     plt.legend(loc="best")
+    plt.xlim(1, 10)
     plt.show()
 
 
